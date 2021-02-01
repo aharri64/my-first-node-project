0afe697689ca2005982aaee1ecbb422ff964718f
aharri64 <haveacompleteday@gmail.com>
2021-02-01 12:11:23 -0700

    added moments from npm

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-//how to import module
-const myModule = require('./myModule');
-const myOtherModule = require('./myOtherModule');
-const thirdModule = require('./thirdModule');
+// //how to import module
+// const myModule = require('./myModule');
+// const myOtherModule = require('./myOtherModule');
+// const thirdModule = require('./thirdModule');
 
-console.log(thirdModule.multiply(2, 2))
-
-
-console.log("Hello World")
-function hello(name) {
-    console.log(`Hello, ${name}`);
-}
-
-hello('Amir');
+// console.log(thirdModule.multiply(2, 2))
 
 
-//invoking the function in the module
-console.log(myModule.add(2, 2));
-console.log(myModule.subtract(5, 4));
+// console.log("Hello World")
+// function hello(name) {
+//     console.log(`Hello, ${name}`);
+// }
 
-//my other module
-myOtherModule.welcomeToNode()
+// hello('Amir');
+
+
+// //invoking the function in the module
+// console.log(myModule.add(2, 2));
+// console.log(myModule.subtract(5, 4));
+
+// //my other module
+// myOtherModule.welcomeToNode()
+
+
+
+
+//fs
+
+const fs = require('fs');
+
+fs.readFile('nfl.txt', 'utf8', function (error, data) {
+    if (error) {
+        console.log('You have a problem reading this file');
+    } else {
+        console.log(data);
+    }
+})
